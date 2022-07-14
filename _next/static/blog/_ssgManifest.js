08f26237907f66e140612dfce1653385b405d780
ysuzuki19 <ysuzuki.cpp@gmail.com>
2022-07-14 20:12:44 +0900

    rebuilding site Thu 14 Jul 2022 08:12:44 PM JST

--- _next/static/blog/_ssgManifest.js
+++ _next/static/blog/_ssgManifest.js
@@ -1,1 +1,1 @@
-self.__SSG_MANIFEST=new Set(["\u002Foss","\u002Fabout","\u002Ffeatures","\u002Flibraries","\u002F404","\u002F","\u002Fpost\u002F[slug]","\u002Ftag\u002F[tag]","\u002Fpage\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002Fabout","\u002Ffeatures","\u002Flibraries","\u002Foss","\u002Fpage\u002F[id]","\u002Fpost\u002F[slug]","\u002Ftag\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
